c21c086ace14e2e6a486860e0f49bdc4a78e58a4
czahie <czahie1996@gmail.com>
2018-02-06 23:37:24 +0800

    lab03 with optional questions, Q10 is very hard

--- Lab/lab03/lab03_extra.py
+++ Lab/lab03/lab03_extra.py
@@ -15,9 +15,9 @@ def is_palindrome(n):
     True
     """
     x, y = n, 0
-    f = lambda: _____
+    f = lambda: y * 10 + x % 10
     while x > 0:
-        x, y = _____, f()
+        x, y = x // 10, f()
     return y == n
 
 def skip_mul(n):
@@ -28,7 +28,9 @@ def skip_mul(n):
     >>> skip_mul(8) # 8 * 6 * 4 * 2
     384
     """
-    if n == 2:
+    if n == 1:
+        return 1
+    elif n == 2:
         return 2
     else:
         return n * skip_mul(n - 2)
@@ -45,6 +47,9 @@ def count_up(n):
     """
     def counter(i):
         "*** YOUR CODE HERE ***"
+        if i <= n:
+            print(i)
+            counter(i + 1)
     counter(1)
 
 def is_prime(n):
@@ -58,6 +63,15 @@ def is_prime(n):
     True
     """
     "*** YOUR CODE HERE ***"
+    def calculate(i):
+        if i < n:
+            if n ** 0.5 % i == 0:  # 'n ** 0.5' could be replaced by 'n'
+                return False
+            else:
+                return calculate(i + 1)
+        else:
+            return True
+    return calculate(2)
 
 def interleaved_sum(n, odd_term, even_term):
     """Compute the sum odd_term(1) + even_term(2) + odd_term(3) + ..., up
@@ -68,6 +82,23 @@ def interleaved_sum(n, odd_term, even_term):
     29
     """
     "*** YOUR CODE HERE ***"
+# My solution
+    def helper(i):
+        total = 0
+        if i + 1 <= n:
+            return total + odd_term(i) + even_term(i + 1) + helper(i + 2)
+        elif i <= n:
+            return total + odd_term(i) + helper(i + 1)
+        else:
+            return total
+    return helper(1)
+
+# Official solution
+    def helper(term0, term1, k):
+        if k == n:
+            return term0(k)
+        return term0(k) + helper(term1, term0, k + 1)
+    return helper(odd_term, even_term, 1)
 
 def ten_pairs(n):
     """Return the number of ten-pairs within positive integer n.
@@ -80,3 +111,20 @@ def ten_pairs(n):
     6
     """
     "*** YOUR CODE HERE ***"
+# Could not figure it our until I saw the official solution
+# "Do not use assignment statements" makes this question more difficult
+    if n < 10:
+        return 0
+    else:
+        return ten_pairs(n // 10) + count_digit(n // 10, 10 - n % 10)
+
+def count_digit(n, digit):
+    if n == 0:
+        return 0
+    elif n % 10 == digit:
+        return count_digit(n // 10, digit) + 1
+    else:
+        return count_digit(n // 10, digit)
+
+
+
